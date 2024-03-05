fe42fa28cb2eb4349a5df3fe264eba81df14fbb8
B00155926 <b00155926@mytudublin.ie>
2024-03-05 08:31:20 +0000

    Update config.php

--- The Candy Shop/public/src/config.php
+++ The Candy Shop/public/src/config.php
@@ -5,7 +5,7 @@
  */
 $host = "localhost";
 $username = "root";
-$password = "Gunners@1987";
+$password = "Strigatare!1";
 $dbname = "The_Candy_Shop"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
