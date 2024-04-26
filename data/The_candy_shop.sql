e5a46c937ab5255c75145490cb4b6560bec75843
Mirabela Ghiuzan <145861242+B00155926@users.noreply.github.com>
2024-04-26 13:54:39 +0100

    Add files via upload

---
+++ data/The_candy_shop.sql
@@ -0,0 +1,155 @@
+LastSessions<|||>1<|||>Laragon.MySQL
+LastActiveSession<|||>1<|||>Laragon.MySQL
+ColWidths_connform.ListSessions<|||>1<|||>163,50,50,50,50,50,10
+ColsVisible_connform.ListSessions<|||>1<|||>0,1,4,6
+ColPositions_connform.ListSessions<|||>1<|||>0,1,2,3,4,5,6
+ColSort_connform.ListSessions<|||>1<|||>0,0
+MainWinOnMonitor<|||>3<|||>0
+ColWidths_MainForm.ListDatabases<|||>1<|||>150,80,50,50,50,50,50,50,50,50,120
+ColsVisible_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColPositions_MainForm.ListDatabases<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10
+ColSort_MainForm.ListDatabases<|||>1<|||>0,0
+ColWidths_MainForm.ListVariables<|||>1<|||>160,200,428
+ColsVisible_MainForm.ListVariables<|||>1<|||>0,1,2
+ColPositions_MainForm.ListVariables<|||>1<|||>0,1,2
+ColSort_MainForm.ListVariables<|||>1<|||>0,0
+ColWidths_MainForm.ListStatus<|||>1<|||>160,428,100,100
+ColsVisible_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColPositions_MainForm.ListStatus<|||>1<|||>0,1,2,3
+ColSort_MainForm.ListStatus<|||>1<|||>0,0
+ColWidths_MainForm.ListProcesses<|||>1<|||>70,80,80,80,80,50,50,210
+ColsVisible_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColPositions_MainForm.ListProcesses<|||>1<|||>0,1,2,3,4,5,6,7
+ColSort_MainForm.ListProcesses<|||>1<|||>0,1
+ColWidths_MainForm.ListCommandStats<|||>1<|||>120,100,100,100,368
+ColsVisible_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColPositions_MainForm.ListCommandStats<|||>1<|||>0,1,2,3,4
+ColSort_MainForm.ListCommandStats<|||>1<|||>1,1
+ColWidths_MainForm.ListTables<|||>1<|||>120,70,70,120,120,70,100,50,70,70,70,70,70,90,120,70,70,70,50
+ColsVisible_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,18
+ColPositions_MainForm.ListTables<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18
+ColSort_MainForm.ListTables<|||>1<|||>0,0
+MainWinWidth<|||>3<|||>1173
+MainWinHeight<|||>3<|||>497
+SessionManager_WindowLeft<|||>3<|||>737
+SessionManager_WindowTop<|||>3<|||>85
+TabCloseOnDoubleClick<|||>3<|||>0
+ColWidths_frmTableEditor.listColumns<|||>1<|||>57,100,152,90,60,65,50,116,130,137,100,100
+ColsVisible_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColPositions_frmTableEditor.listColumns<|||>1<|||>0,1,2,3,4,5,6,7,8,9,10,11
+ColSort_frmTableEditor.listColumns<|||>1<|||>-1,0
+ColWidths_frmTableEditor.treeIndexes<|||>1<|||>502,100,80,120
+ColsVisible_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColPositions_frmTableEditor.treeIndexes<|||>1<|||>0,1,2,3
+ColSort_frmTableEditor.treeIndexes<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listForeignKeys<|||>1<|||>389,80,100,80,80,80
+ColsVisible_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColPositions_frmTableEditor.listForeignKeys<|||>1<|||>0,1,2,3,4,5
+ColSort_frmTableEditor.listForeignKeys<|||>1<|||>-1,0
+ColWidths_frmTableEditor.listCheckConstraints<|||>1<|||>200,609
+ColsVisible_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColPositions_frmTableEditor.listCheckConstraints<|||>1<|||>0,1
+ColSort_frmTableEditor.listCheckConstraints<|||>1<|||>-1,0
+querymemoheight<|||>3<|||>121
+dbtreewidth<|||>3<|||>394
+MainWinLeft<|||>3<|||>24
+MainWinTop<|||>3<|||>78
+sqloutheight<|||>3<|||>30
+SQLFile0<|||>1<|||>C:\Users\mirab\OneDrive - Technological University Dublin\Documents\Documents\Web Developement Server-Side\Sessions\query.sql
+SQLFile1<|||>1<|||>C:\laragon\bin\heidisql\Backups\query-tab-2024-03-09_17-06-56-093.sql
+Servers\Laragon.MySQL\SessionCreated<|||>1<|||>2022-09-09 20:36:07
+Servers\Laragon.MySQL\Host<|||>1<|||>127.0.0.1
+Servers\Laragon.MySQL\WindowsAuth<|||>3<|||>0
+Servers\Laragon.MySQL\CleartextPluginEnabled<|||>3<|||>0
+Servers\Laragon.MySQL\User<|||>1<|||>root
+Servers\Laragon.MySQL\Password<|||>1<|||>2
+Servers\Laragon.MySQL\LoginPrompt<|||>3<|||>0
+Servers\Laragon.MySQL\Port<|||>1<|||>3306
+Servers\Laragon.MySQL\NetType<|||>3<|||>0
+Servers\Laragon.MySQL\Compressed<|||>3<|||>0
+Servers\Laragon.MySQL\LocalTimeZone<|||>3<|||>0
+Servers\Laragon.MySQL\QueryTimeout<|||>3<|||>30
+Servers\Laragon.MySQL\KeepAlive<|||>3<|||>20
+Servers\Laragon.MySQL\FullTableStatus<|||>3<|||>1
+Servers\Laragon.MySQL\Databases<|||>1<|||>
+Servers\Laragon.MySQL\Library<|||>1<|||>libmysql.dll
+Servers\Laragon.MySQL\Comment<|||>1<|||>
+Servers\Laragon.MySQL\StartupScriptFilename<|||>1<|||>
+Servers\Laragon.MySQL\TreeBackground<|||>3<|||>536870911
+Servers\Laragon.MySQL\SshExecutable<|||>1<|||>plink.exe
+Servers\Laragon.MySQL\SSHtunnelHost<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelHostPort<|||>3<|||>22
+Servers\Laragon.MySQL\SSHtunnelUser<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelPassword<|||>1<|||>2
+Servers\Laragon.MySQL\SSHtunnelTimeout<|||>3<|||>4
+Servers\Laragon.MySQL\SSHtunnelPrivateKey<|||>1<|||>
+Servers\Laragon.MySQL\SSHtunnelPort<|||>3<|||>3307
+Servers\Laragon.MySQL\SSL_Active<|||>3<|||>0
+Servers\Laragon.MySQL\SSL_Key<|||>1<|||>
+Servers\Laragon.MySQL\SSL_Cert<|||>1<|||>
+Servers\Laragon.MySQL\SSL_CA<|||>1<|||>
+Servers\Laragon.MySQL\SSL_Cipher<|||>1<|||>
+Servers\Laragon.MySQL\IgnoreDatabasePattern<|||>1<|||>
+Servers\Laragon.MySQL\LogFileDdl<|||>3<|||>0
+Servers\Laragon.MySQL\LogFileDml<|||>3<|||>0
+Servers\Laragon.MySQL\LogFilePath<|||>1<|||>C:\Users\Administrator\AppData\Roaming\HeidiSQL\Logs\%session\%db\%y%m%d.sql
+Servers\Laragon.MySQL\RefusedCount<|||>3<|||>11
+Servers\Laragon.MySQL\ServerVersionFull<|||>1<|||>8.0.34 - MySQL Community Server - GPL
+Servers\Laragon.MySQL\ConnectCount<|||>3<|||>51
+Servers\Laragon.MySQL\ServerVersion<|||>3<|||>80034
+Servers\Laragon.MySQL\LastConnect<|||>1<|||>2024-04-26 13:52:19
+Servers\Laragon.MySQL\lastUsedDB<|||>1<|||>the_candy_shop
+Servers\Laragon.MySQL\QueryHistory\33<|||>1<|||>2024-03-25 12:15:17|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE orders ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\35<|||>1<|||>2024-03-25 12:23:02|the_candy_shop|16|USE the_candy_shop;<{{{><}}}> TABLE product ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\42<|||>1<|||>2024-03-25 12:56:26|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE inventory_pages ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\36<|||>1<|||>2024-03-25 13:03:51|the_candy_shop|172|USE the_candy_shop;<{{{><}}}> ALTER TABLE Orders<{{{><}}}>ADD COLUMN product_id INT,<{{{><}}}>ADD COLUMN quantity INT,<{{{><}}}>ADD FOREIGN KEY (product_id) REFERENCES Product(product_id);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\43<|||>1<|||>2024-03-25 13:04:15|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Orders;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\45<|||>1<|||>2024-03-25 13:44:19|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE product;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\44<|||>1<|||>2024-03-25 13:57:50|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE admin_product;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\46<|||>1<|||>2024-03-25 18:12:15|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE inventory_pages;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\47<|||>1<|||>2024-03-25 18:17:56|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE inventory_pages;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>INSERT INTO inventory_pages (content) VALUES ('Initial content for testing');<{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\49<|||>1<|||>2024-03-25 18:29:19|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE inventory_pages;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\48<|||>1<|||>2024-03-25 18:39:58|the_candy_shop|16|USE the_candy_shop;<{{{><}}}> TABLE Orders;<{{{><}}}>    <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\50<|||>1<|||>2024-03-25 18:42:32|the_candy_shop|109|USE the_candy_shop;<{{{><}}}>drop TABLE Orders;<{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\51<|||>1<|||>2024-03-25 18:43:52|the_candy_shop|31|USE the_candy_shop;<{{{><}}}>CREATE TABLE Orders (<{{{><}}}>    order_id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,<{{{><}}}>    product_name VARCHAR(255),<{{{><}}}>    quantity INT,<{{{><}}}>    total_products_available INT<{{{><}}}>);<{{{><}}}><{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\55<|||>1<|||>2024-03-25 20:09:04|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE product;<{{{><}}}><{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\52<|||>1<|||>2024-03-25 20:40:27|the_candy_shop|47|USE the_candy_shop;<{{{><}}}> drop TABLE Orders ;<{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\56<|||>1<|||>2024-03-25 20:41:00|the_candy_shop|94|USE the_candy_shop;<{{{><}}}> CREATE TABLE Orders (<{{{><}}}>    order_id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,<{{{><}}}>    product_id INT,<{{{><}}}>    quantity INT,<{{{><}}}>    FOREIGN KEY (product_id) REFERENCES Product(product_id)<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\57<|||>1<|||>2024-03-25 20:41:18|the_candy_shop|15|USE the_candy_shop;<{{{><}}}>  TABLE Orders;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\58<|||>1<|||>2024-03-25 20:51:50|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE orders;<{{{><}}}><{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\54<|||>1<|||>2024-03-25 20:52:12|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Orders ;<{{{><}}}><{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\53<|||>1<|||>2024-03-25 20:55:10|the_candy_shop|16|USE the_candy_shop;<{{{><}}}> drop TABLE orders ;<{{{><}}}>   <{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\59<|||>1<|||>2024-03-25 20:55:42|the_candy_shop|15|USE the_candy_shop;<{{{><}}}> CREATE TABLE orders (<{{{><}}}>                        order_id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>                        order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,<{{{><}}}>                        product_name VARCHAR(255),<{{{><}}}>                        quantity INT,<{{{><}}}>                        total_products_available INT<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\60<|||>1<|||>2024-03-26 13:00:30|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>  TABLE orders ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\61<|||>1<|||>2024-03-26 13:41:52|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>  TABLE product ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\1<|||>1<|||>2024-03-26 15:06:59|the_candy_shop|47|USE the_candy_shop;<{{{><}}}>  TABLE profile ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\2<|||>1<|||>2024-03-26 15:07:09|the_candy_shop|172|USE the_candy_shop;<{{{><}}}> drop TABLE profile ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\62<|||>1<|||>2024-03-26 15:07:23|the_candy_shop|172|USE the_candy_shop;<{{{><}}}>CREATE TABLE Profile (<{{{><}}}>    profile_id INT AUTO_INCREMENT PRIMARY KEY,<{{{><}}}>    user_id INT UNIQUE,<{{{><}}}>    firstname VARCHAR(255),<{{{><}}}>    lastname VARCHAR(255),<{{{><}}}>    address VARCHAR(255),<{{{><}}}>    email VARCHAR(255),<{{{><}}}>    telephone VARCHAR(20),<{{{><}}}>    role VARCHAR(20),<{{{><}}}>    username VARCHAR(255),<{{{><}}}>    password VARCHAR(255),<{{{><}}}>    FOREIGN KEY (user_id) REFERENCES User(user_id)<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\64<|||>1<|||>2024-03-26 15:28:56|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Profile ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\63<|||>1<|||>2024-03-26 15:29:44|the_candy_shop|15|USE the_candy_shop;<{{{><}}}> TABLE user ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\65<|||>1<|||>2024-03-26 15:30:04|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE profile ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\66<|||>1<|||>2024-03-26 15:36:45|the_candy_shop|78|USE the_candy_shop;<{{{><}}}> ALTER TABLE Profile ADD COLUMN processed BOOLEAN DEFAULT FALSE;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\68<|||>1<|||>2024-03-26 16:16:50|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Profile;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\67<|||>1<|||>2024-03-26 17:08:37|the_candy_shop|16|USE the_candy_shop;<{{{><}}}> TABLE employee;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\69<|||>1<|||>2024-03-26 17:08:46|the_candy_shop|62|USE the_candy_shop;<{{{><}}}>drop TABLE employee;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\70<|||>1<|||>2024-03-26 17:08:57|the_candy_shop|109|USE the_candy_shop;<{{{><}}}>CREATE TABLE Employee (<{{{><}}}>    employee_id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>    user_id INT UNIQUE,<{{{><}}}>    daily_sales_date DATE,<{{{><}}}>    total_sales DECIMAL(10, 2),<{{{><}}}>    FOREIGN KEY (user_id) REFERENCES User(user_id)<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\71<|||>1<|||>2024-03-26 17:09:13|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE Employee;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\72<|||>1<|||>2024-03-26 17:15:04|the_candy_shop|31|USE the_candy_shop;<{{{><}}}>DROP TABLE Employee;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\75<|||>1<|||>2024-03-26 17:19:53|the_candy_shop|15|USE the_candy_shop;<{{{><}}}>DROP<{{{><}}}> TABLE Employee ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\76<|||>1<|||>2024-03-26 17:20:03|the_candy_shop|79|USE the_candy_shop;<{{{><}}}>CREATE TABLE Employee (<{{{><}}}>    employee_id INT PRIMARY KEY AUTO_INCREMENT,<{{{><}}}>    sale_date DATE,<{{{><}}}>    product_id INT,<{{{><}}}>    quantity_sold INT,<{{{><}}}>    total_sales DECIMAL(10, 2),<{{{><}}}>    FOREIGN KEY (product_id) REFERENCES Product(product_id)<{{{><}}}>);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\73<|||>1<|||>2024-03-26 17:28:25|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Employee ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\78<|||>1<|||>2024-04-08 10:15:02|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE product ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\77<|||>1<|||>2024-04-08 10:15:52|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE orders ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\74<|||>1<|||>2024-04-09 16:59:53|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE inventory_pages ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\5<|||>1<|||>2024-04-15 09:55:52|the_candy_shop|16|USE the_candy_shop;<{{{><}}}> TABLE profile ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\6<|||>1<|||>2024-04-18 13:47:50|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE user ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\3<|||>1<|||>2024-04-19 18:38:50|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE customer ;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\4<|||>1<|||>2024-04-19 18:41:30|the_candy_shop|94|USE the_candy_shop;<{{{><}}}> ALTER TABLE Customer ADD COLUMN firstname VARCHAR(50);<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\8<|||>1<|||>2024-04-19 18:43:12|the_candy_shop|0|USE the_candy_shop;<{{{><}}}> TABLE Customer;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\7<|||>1<|||>2024-04-19 18:45:19|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>ALTER TABLE Customer MODIFY COLUMN firstname VARCHAR(50) DEFAULT NULL;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\9<|||>1<|||>2024-04-19 18:45:36|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE Customer;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\15<|||>1<|||>2024-04-23 13:18:28|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE user;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\13<|||>1<|||>2024-04-23 14:07:46|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE inventory_pages;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\11<|||>1<|||>2024-04-23 14:08:42|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE orders;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\10<|||>1<|||>2024-04-23 14:26:55|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE product;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
+Servers\Laragon.MySQL\QueryHistory\14<|||>1<|||>2024-04-23 14:34:00|the_candy_shop|0|USE the_candy_shop;<{{{><}}}>TABLE profile;<{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}><{{{><}}}>
